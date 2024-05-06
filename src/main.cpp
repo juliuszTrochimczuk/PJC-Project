2040933617367327b300613a7ad1d1d936746b3b
Juliusz Trochimczuk <assasindal@gmail.com>
2024-05-06 13:24:28 +0200

    Setting up the SFML library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include <fmt/core.h>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto window = sf::RenderWindow(sf::VideoMode(800, 600), "Killer Bean");
+    while (window.isOpen()) {
+        auto event = sf::Event();
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
     return 0;
 }
